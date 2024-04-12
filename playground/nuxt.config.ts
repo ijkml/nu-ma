e4523ebe6a9fde6655df63ec38f205678a67e82b
ML <me.mlaure@gmail.com>
2024-04-12 13:03:59 +0100

    chore: playground

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,8 +1,8 @@
 export default defineNuxtConfig({
   modules: ['../src/module'],
   umami: {
-    // host: 'https://savory.vercel.app/',
-    // id: '84cc2d28-8689-4df0-b575-2202e34a75aa',
+    host: 'https://savory.vercel.app/',
+    id: '84cc2d28-8689-4df0-b575-2202e34a75aa',
     autoTrack: true,
     version: 2,
     useDirective: true,
